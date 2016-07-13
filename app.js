024cd49504c31a3679411c9462314a55e4d830c1
Marcelo Nascimento Menezes <mmenezes168@gmail.com>
2016-07-13 16:54:18 -0300

    Added express

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 /*
-*   Created by Marcelo Nascimento Menezes.
-*   Description: Main express app
-*
-*
-* */
+ *   Created by Marcelo Nascimento Menezes.
+ *   Description: Main express app
+ *
+ *
+ * */
+var express = require('express');
+var app = express();
 
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-res.setHeader('Content-Type', 'text/plain');
-res.end('Hello World\n');
+app.get('/', function (req, res) {
+    res.send('Hello World!!!');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/blocks', function (req, res) {
+    var blocks = ['Fixed', 'Movable', 'Rotating'];
+    res.send(blocks);
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
 });
