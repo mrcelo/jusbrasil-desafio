0bf62260667e2aa1067d9945fd48395f80519335
Marcelo Nascimento Menezes <mmenezes168@gmail.com>
2016-07-13 17:30:14 -0300

    Testing bug with git

--- app.js
+++ app.js
@@ -7,14 +7,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!!!');
-});
+app.use(express.static('public'));
 
-app.get('/blocks', function (req, res) {
-    var blocks = ['Fixed', 'Movable', 'Rotating'];
-    res.send(blocks);
-});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
