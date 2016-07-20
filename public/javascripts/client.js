dd6c79a4e4f7ddcc32e6a463df866734c1218f55
Marcelo Nascimento Menezes <mmenezes168@gmail.com>
2016-07-20 15:57:55 -0300

    prettied frontend (html, css, client.js)

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -6,14 +6,30 @@
 $(function () {
 
     var APIURL =  "http://localhost:3000/entities/";
-
+    var total  = 0;
     $('#myform').submit(function ( event ) {
         event.preventDefault();
-        var searchTerms = $('#searchTerm').val();
+        $('ol').empty();
+        var searchTerms = $('.searchTerm').val();
         if ( searchTerms.length ) {
-            $('#searchTerm').val("");
+            $('.searchTerm').val("");
 
         }
+        //else {
+        //     $.ajax({
+        //         type:        "GET",
+        //         url:         APIURL,
+        //         contentType: "application/json; charset=utf-8",
+        //         cache:       false,
+        //         success:     function ( data ) {
+        //
+        //             data.hits.map(function ( hit ) {
+        //                 console.log(hit._source.title);
+        //                 $('ul').append('<li>' + hit._source.title + '</li>');
+        //             });
+        //         }
+        //     });
+        // }
 
         $.ajax({
             type:        "GET",
@@ -23,16 +39,28 @@ $(function () {
             cache:       false,
             success:     function ( data ) {
 
+                if ( data.total == 0 ) {
+                    total = 0;
+                    $('ol').append('<h3>No results.</h3>')
+                } else {
+
+                    $('ol').append('<h3>Results (' + data.total + ')</h3>')
+                    total = data.total;
+
+                }
+
+
                 data.hits.map(function ( hit ) {
                     console.log(hit._source.title);
-                    $('ul').append('<li>' + hit._source.title + '</li>');
+
+                    $('ol').append('<li>' + hit._source.title + '<span class="type">' + hit._source.entitytype + '</span>' + '</li>');
                 });
             }
         });
 
     });
 
-    $("#searchTerm").autocomplete({
+    $(".searchTerm").autocomplete({
         source:    function ( request, response ) {
 
             $.ajax({
@@ -42,10 +70,11 @@ $(function () {
                 data:        { "q": request.term },
                 cache:       false,
                 success:     function ( data ) {
+                    total = data.total;
                     response($.map(data.hits, function ( item ) {
                         return {
                             label: item._source.title,
-                            value: item._source.title
+                            value: item._source.entitytype
                         }
                     }));
 
@@ -54,7 +83,10 @@ $(function () {
         },
         minLength: 2,
         select:    function ( event, ui ) {
-            log(ui.item ? "Selected: " + ui.item.label : "Nothing selected, input was " + this.value);
+            // log(ui.item ? "Selected: " + ui.item.label : "Nothing selected, input was " + this.value);
+            $('ol').empty();
+            $('ol').append('<h3>Results (' + total + ')</h3>');
+            $('ol').append('<li>' + ui.item.label + '<span class="type">' + ui.item.value + '</span>' + '</li>');
         }
     })
 
