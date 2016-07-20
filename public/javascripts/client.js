803e891b86baed2e83e3c1fcbeea06f2036aa1d9
Marcelo Nascimento Menezes <mmenezes168@gmail.com>
2016-07-20 16:55:38 -0300

    bugs

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -5,7 +5,7 @@
 
 $(function () {
 
-    var APIURL = "https://searchngin.herokuapps.com/entities";
+    var APIURL = "https://searchngin.herokuapp.com/entities";
     var total  = 0;
     $('#myform').submit(function ( event ) {
         event.preventDefault();
